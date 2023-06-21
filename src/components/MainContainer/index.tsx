5d1cdb3b1ff251267a27816df2a1a70726768019
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-06-21 16:51:23 -0300

    MainContainer Component

---
+++ src/components/MainContainer/index.tsx
@@ -0,0 +1,13 @@
+import * as styled from './styled';
+
+export type MainContainerProps = {
+    children: React.ReactNode;
+}
+
+export const MainContainer = ({ children }: MainContainerProps) => {
+  return (
+  <styled.Container>
+    {children}
+  </styled.Container>;
+  );
+};
