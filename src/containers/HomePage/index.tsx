6547f87cb6a06dbf3143f434d334d1ced24596bd
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-06-22 19:11:28 -0300

    Deploy with SSR and SSG on Netlify

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -4,14 +4,16 @@ import { Header } from '../../components/Header';
 import { MainContainer } from '../../components/MainContainer';
 import { PostCard } from '../../components/PostCard';
 import { PostData } from '../../domain/posts/post';
-import { Container } from './styles';
+import { Container, Category } from './styles';
 import { SITE_NAME } from '../../config/app.config';
+
 
 export type HomePageProps = {
   posts: PostData[];
+  category?: string;
 };
 
-export default function HomePage({ posts }: HomePageProps) {
+export default function HomePage({ posts, category }: HomePageProps) {
   return (
     <>
       <Head>
@@ -19,6 +21,7 @@ export default function HomePage({ posts }: HomePageProps) {
         <meta name="description" content="Este é meu blog de tecnologia." />
       </Head>
       <Header />
+      {category && <Category>Categoria: {category}</Category>}
       <MainContainer>
         <Container>
           {posts.map((post) => (
